be6856cfae90ad6412e32cfb2c582961e7e382b6
Maxi Fittipaldi <maxi.fittipald@gmail.com>
2021-10-04 22:19:59 -0300

    comentarios borrados

--- Connection.py
+++ Connection.py
@@ -18,43 +18,10 @@ class Connection():
                     
     def manipulate(self,query, params = ()): #insert, update, delete
         try:
-            # p_id = int(input("product_id: "))
-            # name = input("name: ")
-            # q_in_stock = float(input("quantity_in_stock: "))
-            # unit_price = int(input("unit price: "))
             with self.cnn:
                     with self.cnn.cursor() as cursor:
-                        result = cursor.execute(query,params) # "INSERT INTO products VALUES(%s,%s,%s,%s)",(p_id, name, q_in_stock, unit_price)
+                        result = cursor.execute(query,params)
                         self.cnn.commit()
         except:
             return self.error
-    # def update(self):
-    #     try:
-    #         m_id = int(input("inserte el id del producto que quiere modificar: "))
-    #         mName = input("name: ")
-    #         mQ_in_stock = int(input("quantity_in_stock: "))
-    #         mUnit_price =  float(input("unit price: "))
-    #         with self.cnn:
-    #                 with self.cnn.cursor() as cursor:
-    #                     cursor.execute("""UPDATE products 
-    #                     SET name = %s, 
-    #                         quantity_in_stock = %s, 
-    #                         unit_price = %s 
-    #                     WHERE product_id = %s""",(mName, mQ_in_stock, mUnit_price,m_id))
-    #                     self.cnn.commit()
-    #     except:
-    #         return self.error
-
-    # def delete(self):
-    #     d_id = int(input("inserte el id del producto que quiere eliminar: "))
-    #     with self.cnn:
-    #             with self.cnn.cursor() as cursor:
-    #                 cursor.execute("DELETE FROM products WHERE product_id = %s",(d_id))
-    #                 self.cnn.commit()
-
-    # def search(self):
-    #     with self.cnn:
-    #             with self.cnn.cursor() as cursor:
-    #                 cursor.execute(f'SELECT * FROM products WHERE name LIKE "%{userSearch}%"')
-    #                 return cursor.fetchall()
                     
